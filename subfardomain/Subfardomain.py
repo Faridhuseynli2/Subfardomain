80335d332f06c0904be0d0f1c5d1aaa0bf6c6b64
Farid Huseynli <feridtwitch@proton.me>
2023-06-17 23:29:46 -0700

    subfardomain Finder v1
    Simple Subdomain Discovery Tool
    
    This is a simple Python-based subdomain discovery tool. It allows you to scan valid subdomains using a specific subdomain list on a given root domain. It can be used for accessibility and asset identification of subdomains on the target website.
    
    Usage
    
        This program requires Python language and the requests library to run. Make sure you have these requirements fulfilled.
    
        Next, enter the subdomain list in the text file named mySubDomain.txt located in the same directory as the program. Add one subdomain per line. For example:
    
    mail
    blog
    shop
    test
    
        Run the program and select your language preference. Choose your selection from the supported languages.
    
        Then, enter the target domain. For example: example.com
    
        The program will start scanning the subdomains and will display the valid subdomains it finds.
    
        The results will be shown as a valid domain name on the screen, in the language you have selected.
    
    Important Notes
    
        This tool only shows the results when it receives a valid response from the scanned subdomains. Inaccessible or error-prone subdomains will not be included in the results.
    
        The program may take some time to complete, so please wait for it to finish.
    
        This tool should be used for ethical hacking and authorized testing purposes only. Unauthorized access or damage to systems belonging to others is illegal and can have serious legal consequences. The use of this tool is your responsibility.

---
+++ subfardomain/Subfardomain.py
@@ -0,0 +1,55 @@
+import requests
+
+languages = {
+    "1": "Azerbaijan",
+    "2": "English",
+    "3": "Turkce",
+    "4": "Deutsch", 
+    "5": "Español"  
+}
+
+print("Dil Seçin:")
+for key, value in languages.items():
+    print(f"{key}) {value}")
+
+selected_language = input()
+
+user_agents = {
+    "1": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 Edg/103.0.1264.77",
+    "2": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 Edg/103.0.1264.77",
+    "3": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 Edg/103.0.1264.77",
+    "4": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 Edg/103.0.1264.77",
+    "5": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 Edg/103.0.1264.77"
+}
+
+user_agent = user_agents.get(selected_language)
+
+print("Enter Domain :")
+target_domain = input()
+
+subdomain_list = open("mySubDomain.txt").read()
+subdoms = subdomain_list.splitlines()
+
+for sub in subdoms:
+    domain_test = f"http://{sub}.{target_domain}"
+
+    try:
+        response = requests.get(
+            domain_test,
+            headers={"User-Agent": user_agent},
+        )
+        if response.status_code == 200:
+            if selected_language == "1":
+                print("Domain Tapildi:", domain_test)  # Azerbaijan
+            elif selected_language == "2":
+                print("Valid Domain:", domain_test)  # English
+            elif selected_language == "3":
+                print("Geçerli Alan Adı:", domain_test)  # Türkçe
+            elif selected_language == "4":
+                print("Gültige Domain:", domain_test)  # Almanca
+            elif selected_language == "5":
+                print("Dominio válido:", domain_test)  # İspanyolca
+            else:
+                print("Geçerli Alan Adı:", domain_test)  
+    except requests.exceptions.RequestException:
+        pass
